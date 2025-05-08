c1b10ea6945f225160dc9ff1e9bc60f5a31d9284
jeff <1105041+jeff-dh@users.noreply.github.com>
2025-05-08 21:45:40 +0200

    improved executor, added pending shutdown

--- src/aio_process_pool/executor.py
+++ src/aio_process_pool/executor.py
@@ -10,6 +10,7 @@ class Executor(concurrent.futures.Executor):
     def __init__(self, max_workers=None):
         self._futures_dict = {}
         self._pool = ProcessPool(max_workers, self._set_running_or_notify_cancel)
+        self.pending_shutdown = False
 
     def _set_running_or_notify_cancel(self, task) -> bool:
         assert task in self._futures_dict
@@ -28,28 +29,28 @@ class Executor(concurrent.futures.Executor):
         else:
             concurrent_future.set_result(task.result())
 
+        if self.pending_shutdown and len(self._futures_dict) == 0:
+            self._shutdown()
+
     def submit(self, fn, /, *args, **kwargs):
+        # schedule execution as separate task
         task = asyncio.create_task(self._pool.run(fn, *args, **kwargs))
 
+        # create future
         self._futures_dict[task] = concurrent.futures._base.Future()
 
+        # add task.done callback
         task.add_done_callback(self._task_done_callback)
 
+        # return future
         return self._futures_dict[task]
 
-    def shutdown(self, wait=True, *, cancel_futures=False):
-        if cancel_futures:
-            for futures in self._futures_dict.values():
-                futures.cancel()
-
-        if not wait:
-            raise ValueError("TODO: handle wait=False")
-
-        self._pool.shutdown()
-
     async def map_async(self, fn, *iterables, timeout=None, chunksize=1):
-        assert chunksize == 1
-        assert timeout == None
+        if not chunksize == 1:
+            raise ValueError("chunksize != 1 is not implemented yet")
+        if not timeout == None:
+            raise ValueError("timeout != None is not implemented yet")
+
         loop = asyncio.get_event_loop()
 
         futures = [loop.run_in_executor(self, partial(fn, *args))
@@ -65,3 +66,17 @@ class Executor(concurrent.futures.Executor):
 
         coro = self.map_async(fn, *iterables, timeout=timeout, chunksize=chunksize)
         return loop.run_until_complete(coro)
+
+    def _shutdown(self):
+        self._pool.shutdown()
+
+    def shutdown(self, wait=True, *, cancel_futures=False):
+        if cancel_futures:
+            for futures in self._futures_dict.values():
+                futures.cancel()
+
+        if not wait:
+            self.pending_shutdown = True
+            return
+
+        self._shutdown()
