0bf865f6b5189e4b112c36f4a08c6d3e6b4c17c4
jeff <1105041+jeff-dh@users.noreply.github.com>
2025-05-11 19:02:27 +0200

    moved executor tests into tests/executor_test.py

--- tests/executor_test.py
+++ tests/executor_test.py
@@ -3,10 +3,47 @@ import pytest
 
 from functools import partial
 
-from aio_process_pool import Executor
-from .pool_test import fib
+from aio_process_pool import Executor, SubprocessException
+from .pool_test import fib, first_30_fib_numbers, raise_exception
 
 fib32 = 2178309 # fib(32)
+
+@pytest.mark.asyncio
+async def test_exception_executor():
+    exe = Executor()
+    with pytest.raises(SubprocessException):
+        await exe.map_async(raise_exception, [1])
+    exe.shutdown()
+
+@pytest.mark.asyncio
+async def test_executor():
+    exe = Executor()
+    loop = asyncio.get_event_loop()
+
+    futures = [loop.run_in_executor(exe, partial(fib, i)) for i in range(30)]
+    assert await asyncio.gather(*futures) == first_30_fib_numbers
+
+    await exe.shutdown_async()
+
+@pytest.mark.asyncio
+async def test_async_map():
+    exe = Executor()
+    assert await exe.map_async(fib, range(30)) == first_30_fib_numbers
+    await exe.shutdown_async()
+
+def test_map():
+    exe = Executor()
+    assert exe.map(fib, range(30)) == first_30_fib_numbers
+    exe.shutdown()
+
+def test_context_manager():
+    with Executor() as exe:
+        assert exe.map(fib, range(30)) == first_30_fib_numbers
+
+@pytest.mark.asyncio
+async def test_async_context_manager():
+    async with Executor() as exe:
+        assert await exe.map_async(fib, range(30)) == first_30_fib_numbers
 
 def test_shutdown_trivial():
     exe = Executor()
--- tests/pool_test.py
+++ tests/pool_test.py
@@ -1,10 +1,8 @@
 import asyncio
 import pytest
 
-from functools import partial
 
-from aio_process_pool import ProcessPool, Executor
-from aio_process_pool.utils import SubprocessException
+from aio_process_pool import ProcessPool, SubprocessException
 
 def fib(n):
     assert n >= 0
@@ -12,7 +10,7 @@ def fib(n):
     if n <= 2: return 1
     return fib(n-1) + fib(n-2)
 
-def raise_exception(a=1):
+def raise_exception(_=1):
     assert False
 
 first_30_fib_numbers = [fib(x) for x in range(30)]
@@ -31,47 +29,10 @@ async def test_exception():
         await pool.run(raise_exception)
     pool.shutdown()
 
-@pytest.mark.asyncio
-async def test_exception_executor():
-    exe = Executor()
-    with pytest.raises(SubprocessException):
-        await exe.map_async(raise_exception, [1])
-    exe.shutdown()
-
 def test_0_worker():
     with pytest.raises(ValueError):
         _ = ProcessPool(0)
     assert True
-
-@pytest.mark.asyncio
-async def test_executor():
-    exe = Executor()
-    loop = asyncio.get_event_loop()
-
-    futures = [loop.run_in_executor(exe, partial(fib, i)) for i in range(30)]
-    assert await asyncio.gather(*futures) == first_30_fib_numbers
-
-    await exe.shutdown_async()
-
-@pytest.mark.asyncio
-async def test_async_map():
-    exe = Executor()
-    assert await exe.map_async(fib, range(30)) == first_30_fib_numbers
-    await exe.shutdown_async()
-
-def test_map():
-    exe = Executor()
-    assert exe.map(fib, range(30)) == first_30_fib_numbers
-    exe.shutdown()
-
-def test_context_manager():
-    with Executor() as exe:
-        assert exe.map(fib, range(30)) == first_30_fib_numbers
-
-@pytest.mark.asyncio
-async def test_async_context_manager():
-    async with Executor() as exe:
-        assert await exe.map_async(fib, range(30)) == first_30_fib_numbers
 
 tpi_pool = ProcessPool()
 def blub(): return True
